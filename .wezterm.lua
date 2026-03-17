7ec2341a8990f762b5c7e55c768d0329a38aaeae
ali <alihashemi7@gmail.com>
2026-03-16 17:41:32 -0700

    Update font to berkeley mono

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,8 +3,8 @@ local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 28
-config.font_size = 15
-config.font = wezterm.font("Berkeley Mono Trial")
+config.font_size = 14
+config.font = wezterm.font("Berkeley Mono")
 config.color_scheme = 'tokyonight'
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
